7f262cd5e57421fc4388cfd374ae52cb148c2611
Damjan <damjan_dimitrov@yahoo.com>
2019-07-08 13:04:31 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System.Management;
+using System.Reflection.Emit;
+using System.Linq;
+
+namespace Web_App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+
+
+
+            ManagementObjectSearcher MOS = new ManagementObjectSearcher("Select * From Win32_BaseBoard");
+
+                var arr = MOS.Get();
+                foreach (var item in arr)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+    }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
+
+        
+    }
+}
