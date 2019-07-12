ff4f6436fcd3f9aad034586bb0e3c1d351fe7593
Damjan <damjan_dimitrov@yahoo.com>
2019-07-12 14:54:36 +0200

    Minor changes

--- Program.cs
+++ Program.cs
@@ -8,6 +8,13 @@ using Microsoft.AspNetCore.Hosting;
 using System.Management;
 using System.Reflection.Emit;
 using System.Linq;
+using GemBox.Document;
+using Microsoft.AspNet.Mvc.ViewFeatures;
+using System.Net.Cache;
+using Microsoft.AspNetCore.Http;
+using System.Web;
+using IdentityModel.Client;
+using System.Net;
 
 namespace Web_App
 {
@@ -15,7 +22,19 @@ namespace Web_App
     {
         public static void Main(string[] args)
         {
+          
+            //string path = Environment.CurrentDirectory;
+            //string file = path + "\\web.html";
+            //WebClient client = new WebClient();
+          
+          
+            //string downloadString = client.DownloadString("https://youtube.com");
+            //File.WriteAllText(file, downloadString);
             CreateWebHostBuilder(args).Build().Run();
+
+
+
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
