a0c74d96d595d383f0eb4bafd3452bd9fc8d9b57
Damjan <damjan_dimitrov@yahoo.com>
2019-07-15 09:33:02 +0200

    Fixed namespaces

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Web_App.Pages
+namespace Audit.Pages
 {
     public class PrivacyModel : PageModel
     {
