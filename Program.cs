8e7e2253326b4ac1c30c8e78954ca128855c6b83
Damjan <damjan_dimitrov@yahoo.com>
2019-07-15 09:33:02 +0200

    Fixed namespaces

--- Program.cs
+++ Program.cs
@@ -1,33 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using System.Management;
-using System.Reflection.Emit;
-using System.Linq;
-using GemBox.Document;
-using Microsoft.AspNet.Mvc.ViewFeatures;
-using System.Net.Cache;
-using Microsoft.AspNetCore.Http;
-using System.Web;
-using IdentityModel.Client;
-using System.Net;
 
-namespace Web_App
+namespace Audit
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-          
+
             //string path = Environment.CurrentDirectory;
             //string file = path + "\\web.html";
             //WebClient client = new WebClient();
-          
-          
+
+
             //string downloadString = client.DownloadString("https://youtube.com");
             //File.WriteAllText(file, downloadString);
             CreateWebHostBuilder(args).Build().Run();
--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Web_App
+namespace Audit
 {
     public class Startup
     {
